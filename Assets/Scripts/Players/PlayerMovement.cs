770ea48d5f419106e8c561084ae182d63b9114e2
silverknet <70332569+silverknet@users.noreply.github.com>
2024-09-10 22:28:58 +0200

    feat: simple rotation
    Feat: added simple rotation, and tweaked acceleration and deceleration values

--- Assets/Scripts/Players/PlayerMovement.cs
+++ Assets/Scripts/Players/PlayerMovement.cs
@@ -30,46 +30,40 @@ public class PlayerMovement : MonoBehaviour
     {
         FrameReset();
         HandleDirection();
+        HandleRotation();
         ApplyMovement();
     }
     void FrameReset(){
         _frameVelocity = new Vector3(0.0f,0.0f,0.0f);
     }
-    void HandleDirection(){
+    void HandleDirection()
+    {
         Vector2 input = _inputManager.GetMovementVector();
-        // float inp_x = Mathf.abs(input.x);
-        //float normalized_velocity_x = _velocity.x / GlobalSettings.MAX_SPEED;
-        // float vel_x = Mathf.abs(normalized_velocity_x);
-        // float delta = vel_x < inp_x ? ACCELARATION : DECELERATION;
-        // float direction = Mathf.sign(input.x);
-        // float stopThresh = 0.2f;
-        // float dec = input.x * direction < (normalized_velocity_x * direction - stopThresh) ? GlobalSettings.DECELERATION * direction: 0.0f;
-
 
         input = Vector2.ClampMagnitude(input, 1f);
 
-        Vector2 acceleration = input * GlobalSettings.ACCELARATION;
+        Vector2 acceleration = input * GlobalSettings.ACCELERATION; 
 
         _velocity += acceleration * Time.deltaTime;
 
-        // Apply deceleration when no input
         if (Mathf.Approximately(input.x, 0f))
         {
-            _velocity.x = Mathf.MoveTowards(_velocity.x, 0, GlobalSettings.DECELERATION * Time.deltaTime);
+            _velocity.x = Mathf.MoveTowards(_velocity.x, 0, GlobalSettings.DECELERATION * Time.deltaTime); 
         }
+
         if (Mathf.Approximately(input.y, 0f))
         {
-            _velocity.y = Mathf.MoveTowards(_velocity.y, 0, GlobalSettings.DECELERATION * Time.deltaTime);
+            _velocity.y = Mathf.MoveTowards(_velocity.y, 0, GlobalSettings.DECELERATION * Time.deltaTime); 
         }
 
-        // Clamp velocity to max speed
         _velocity = Vector2.ClampMagnitude(_velocity, GlobalSettings.MAX_SPEED);
 
-
-        // _velocity.x =  Mathf.Min(100.0f, Mathf.Max(-100.0f, _velocity.x + input.x));
-        // _velocity.y = Mathf.Min(100.0f, Mathf.Max(-100.0f, _velocity.y + input.y));
-
         _frameVelocity = _velocity * Time.deltaTime;
+    }
+    void HandleRotation(){
+        Vector3 currentRotation = transform.eulerAngles;
+        currentRotation.z = (_velocity.x / GlobalSettings.MAX_SPEED) * -30.0f;
+        transform.eulerAngles = currentRotation;
     }
 
     private void ApplyMovement() => _rb.velocity = _frameVelocity;
