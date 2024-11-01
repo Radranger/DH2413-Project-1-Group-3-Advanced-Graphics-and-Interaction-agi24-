cfe0d1f6ea8f58743252e1ffcb5eb1800c37729c
jzz1221 <yangk@kth.se>
2024-11-01 12:05:29 +0100

    update
    R button works in every context

--- Assets/Scripts/Networking/ServerManager.cs
+++ Assets/Scripts/Networking/ServerManager.cs
@@ -123,11 +123,8 @@ public class ServerManager : Singleton<ServerManager>
         
         if (Input.GetKeyDown(KeyCode.R))
         {
-            if (gameStarted)
-            {
-                EndGame();
+                if(gameStarted) EndGame();
                 SceneManager.LoadScene("Scenes/RestartPagePC");
-            } 
         }
     }
     
